970ed58e3c8c35d36a07a906664155e565c8e7ee
Redwan Hossain <redwanh25@gmail.com>
2018-12-09 07:24:05 +0600

    update

---
+++ SoftwareForACompany_1/Software_Company_WebApplication/Software_Company_WebApplication/Controllers/PersonController.cs
@@ -0,0 +1,35 @@
+﻿using Software_Company_WebApplication.DatabaseConnection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Software_Company_WebApplication.Controllers
+{
+    public class PersonController : Controller
+    {
+        // GET: Person
+        public ActionResult Index()
+        {
+            PersonBuisnessLayer personBuisnessLayer = new PersonBuisnessLayer();
+            List<PersonMessage> list = personBuisnessLayer.personsIndex.ToList();
+            return View(list);
+        }
+        [HttpGet]
+        public ActionResult Details(int id)
+        {
+            PersonBuisnessLayer personBuisnessLayer = new PersonBuisnessLayer();
+            PersonMessage person = personBuisnessLayer.personsDetails.Single(per => per.Id == id);
+            return View(person);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            PersonBuisnessLayer personBuisnessLayer = new PersonBuisnessLayer();
+            personBuisnessLayer.DeleteEmployee(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ SoftwareForACompany_1/Software_Company_WebApplication/Software_Company_WebApplication/DatabaseConnection/PersonMessage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Software_Company_WebApplication.DatabaseConnection
+{
+    [Table("PersonMessage_tbl")]
+    public class PersonMessage
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string PhoneNumber { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Address { get; set; }
+        [Required]
+        public string Message { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
