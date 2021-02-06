424d0bb903f0913da72eeef676d66fdafd10d526
redwan <redwanh25@gmail.com>
2021-02-07 00:53:06 +0600

    update

---
+++ MVCLoginDemo/MVCLoginDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCLoginDemo.Startup))]
+namespace MVCLoginDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
