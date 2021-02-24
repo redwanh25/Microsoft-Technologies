38996f9e7a237aaf8dcc5d5472093bc454a0d60f
redwan <redwanh25@gmail.com>
2021-02-24 21:20:38 +0600

    update

---
+++ AngularJS_Practice/AngularJS_Practice/Controllers/ExtraController.cs
@@ -0,0 +1,25 @@
+﻿using AngularJS_Practice.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AngularJS_Practice.Controllers
+{
+    public class ExtraController : Controller
+    {
+        // GET: Extra
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(string text, List<ExtraClass> list)
+        {
+
+            return Json(new { data = text });
+        }
+    }
+}
---
+++ AngularJS_Practice/AngularJS_Practice/Models/ExtraClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AngularJS_Practice.Models
+{
+    public class ExtraClass
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
